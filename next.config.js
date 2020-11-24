532bafeaefbad3e8c75ade50725caa6b14cb3b1b
KamilGaribov <kamilgarib@gmail.com>
2020-11-24 14:53:12 +0400

    trainingslash

--- next.config.js
+++ next.config.js
@@ -1,17 +1,18 @@
 module.exports = {
-    exportPathMap: async function (
-      defaultPathMap,
-      { dev, dir, outDir, distDir, buildId }
-    ) {
-      return {
-        '/': { page: '/' },
-        '/gul': { page: '/gul' },
-        '/xonca': { page: '/xonca' },
-        '/marsipan': { page: '/marsipan' },
-        '/vitrin': { page: '/vitrin' },
-        // '/p/hello-nextjs': { page: '/post', query: { title: 'hello-nextjs' } },
-        // '/p/learn-nextjs': { page: '/post', query: { title: 'learn-nextjs' } },
-        // '/p/deploy-nextjs': { page: '/post', query: { title: 'deploy-nextjs' } },
-      }
-    },
-  }
+  trailingSlash: true,
+  exportPathMap: async function (
+    defaultPathMap,
+    { dev, dir, outDir, distDir, buildId }
+  ) {
+    return {
+      "/": { page: "/" },
+      "/gul": { page: "/gul" },
+      "/xonca": { page: "/xonca" },
+      "/marsipan": { page: "/marsipan" },
+      "/vitrin": { page: "/vitrin" },
+      // '/p/hello-nextjs': { page: '/post', query: { title: 'hello-nextjs' } },
+      // '/p/learn-nextjs': { page: '/post', query: { title: 'learn-nextjs' } },
+      // '/p/deploy-nextjs': { page: '/post', query: { title: 'deploy-nextjs' } },
+    };
+  },
+};
