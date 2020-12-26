39477536bad6e629507fb989f259173a10127944
KamilGaribov <kamilgarib@gmail.com>
2020-12-26 19:20:00 +0400

    payment done

--- next.config.js
+++ next.config.js
@@ -14,12 +14,4 @@ module.exports = {
       "/sebet": {page: "/sebet"},
     };
   },
-  webpackDevMiddleware: (config) => {
-    // Solve compiling problem via vagrant
-    config.watchOptions = {
-      poll: 1000,   // Check for changes every second
-      aggregateTimeout: 300,   // delay before rebuilding
-    };
-    return config;
-  }
 };
