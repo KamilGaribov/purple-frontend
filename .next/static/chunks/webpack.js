8ff4a8036a786bb4f145059d094cbd0aba39597d
KamilGaribov <kamilgarib@gmail.com>
2021-01-12 23:15:49 +0400

    production

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "8cf9e4dd027c619b2662";
+/******/ 	var hotCurrentHash = "8785a27ed13c4456bf5b";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
