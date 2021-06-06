34a28bffd5eac791573b825b06a769010020a3cc
Kamil <kamilgarib@gmail.com>
2021-06-06 20:49:15 +0400

    06/06

--- components/variables.js
+++ components/variables.js
@@ -2,5 +2,7 @@ export const Marker = () => <img src="/img/map_marker.png" className="google-map
 export const perPage = 12
 
 
+// export const apiUrl = "http://localhost:8000/"
+export const apiUrl = "http://46.101.106.253:8000/"
 // export const apiUrl = "http://192.168.31.51:8000/"
-export const apiUrl = "https://api.purplecakeboutique.az/"
+// export const apiUrl = "https://api.purplecakeboutique.az/"
